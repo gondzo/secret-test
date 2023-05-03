e37f0f4d6e548f612aed068f6d3ca3460daae82b
gondzo <gondzo@gmail.com>
2023-05-03 11:56:01 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+const accessKey = "AKIASHQWHDZ6DM7YXWNX";
+const secretAccessKey = "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";
+const gitlabToken = "glpat-PQxy2D6edVWeLRJiFLGx";
+const unique="GithubG0ndz0T3stUnique";
